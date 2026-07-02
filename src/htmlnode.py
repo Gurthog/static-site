3511ec67072fd70526bdff06957d281f6e00a82c
Gurthog <226201902+Gurthog@users.noreply.github.com>
2026-07-01 20:46:31 -0600

    add parentnode and leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,3 +25,51 @@ class HTMLNode:
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
+
+class LeafNode(HTMLNode):
+    def __init__(
+            self,
+            tag: str,
+            value: str,
+            props: dict = None,
+        ) -> None:
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self) -> str:
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value.")
+
+        if not self.tag:
+            return self.value
+
+        props = self.props_to_html()
+        return f"<{self.tag}{props}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(
+            self,
+            tag: str,
+            children: list[HTMLNode],
+            props: dict = None,
+        ) -> None:
+        super().__init__(tag, children=children, props=props)
+
+    def to_html(self) -> str:
+        if not self.tag:
+            raise ValueError("All parent nodes must have a tag.")
+        if self.children is None:
+            raise ValueError("Children missing. Should someone call 911?")
+
+        props = self.props_to_html()
+        html = f"<{self.tag}{props}>"
+
+        for node in self.children:
+            html += node.to_html()
+
+        html += f"</{self.tag}>"
+        return html
+
